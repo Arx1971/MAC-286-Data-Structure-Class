c87d79fde461f269e05ca835eba22e72e8e98891
Adnan Rahin <arx.1971@gmail.com>
2019-03-28 12:18:41 -0400

    Doubly_Linked_List

---
+++ src/doubly_linked_list/DoublyLinkedList.java
@@ -0,0 +1,123 @@
+package doubly_linked_list;
+
+
+import java.util.NoSuchElementException;
+
+public class DoublyLinkedList<AnyType> {
+
+	private Node head;
+	private Node tail;
+	private int size;
+
+	public DoublyLinkedList() {
+		size = 0;
+	}
+
+	private class Node {
+		AnyType element;
+		Node next;
+		Node prev;
+
+		public Node(AnyType element, Node next, Node prev) {
+			this.element = element;
+			this.next = next;
+			this.prev = prev;
+		}
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public void addFirst(AnyType element) {
+		Node newNode = new Node(element, head, null);
+		if (head != null) {
+			head.prev = newNode;
+		}
+		head = newNode;
+		if (tail == null) {
+			tail = newNode;
+		}
+		size++;
+		System.out.println("adding: " + element);
+	}
+
+	public void addLast(AnyType element) {
+
+		Node newNode = new Node(element, null, tail);
+		if (tail != null) {
+			tail.next = newNode;
+		}
+		tail = newNode;
+		if (head == null) {
+			head = newNode;
+		}
+		size++;
+		System.out.println("adding: " + element);
+	}
+
+	public void iterateForward() {
+
+		System.out.println("iterating forward..");
+		Node newNode = head;
+		while (newNode != null) {
+			if (newNode.next != null)
+				System.out.print(newNode.element + " -> ");
+			else
+				System.out.println(newNode.element);
+			newNode = newNode.next;
+		}
+	}
+
+	public void iterateBackward() {
+
+		System.out.println("iterating backword..");
+		Node newNode = tail;
+		while (newNode != null) {
+			if (newNode.prev != null)
+				System.out.print(newNode.element + " -> ");
+			else
+				System.out.println(newNode.element);
+			newNode = newNode.prev;
+		}
+	}
+
+	public AnyType removeFirst() {
+		if (size == 0)
+			throw new NoSuchElementException();
+		Node newNode = head;
+		head = head.next;
+		head.prev = null;
+		size--;
+		System.out.println("deleted: " + newNode.element);
+		return newNode.element;
+	}
+
+	public AnyType removeLast() {
+		if (size == 0)
+			throw new NoSuchElementException();
+		Node newNode = tail;
+		tail = tail.prev;
+		tail.next = null;
+		size--;
+		System.out.println("deleted: " + newNode.element);
+		return newNode.element;
+	}
+
+	public static void main(String argv[]) {
+
+		DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
+		dll.addFirst(10);
+		dll.addFirst(34);
+		dll.addLast(56);
+		dll.addLast(364);
+		dll.iterateForward();
+		dll.removeFirst();
+		dll.removeLast();
+		dll.iterateBackward();
+	}
+}
